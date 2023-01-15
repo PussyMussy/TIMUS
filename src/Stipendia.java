46e29cfc6a757f2f2d3013d2d22bc7c483d6efb8
PussyMussy <MaxWar228@mail.ru>
2023-01-15 19:31:33 +0500

    Commit

--- src/Stipendia.java
+++ src/Stipendia.java
@@ -35,5 +35,4 @@ public class Stipendia {
 
         System.out.println(result);
     }
-
 }
--- src/Vibori.java
+++ src/Vibori.java
@@ -2,22 +2,17 @@ import java.io.*;
 import java.text.DecimalFormat;
 import java.util.Locale;
 public class Vibori {
-
-
     static BufferedReader buffer = new BufferedReader(new InputStreamReader(System.in));
     static StreamTokenizer input = new StreamTokenizer(buffer);
     static PrintWriter out = new PrintWriter(System.out);
     static DecimalFormat form = new DecimalFormat("0.00%");
-
     public static int nextInt() throws IOException {
         input.nextToken();
         return (int) input.nval;
     }
-
     public static void printPercent(double value) {
         out.println(form.format(value));
     }
-
     public static void main(String str[]) throws IOException {
         Locale.setDefault(Locale.US);
 
